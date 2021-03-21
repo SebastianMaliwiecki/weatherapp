d99ccf8831b0c43b1363d6beb73da3624a350c5a
Sebastian Maliwiecki <60139438+SebastianMaliwiecki@users.noreply.github.com>
2021-03-21 21:06:10 +0000

    HTML update

--- src/App.js
+++ src/App.js
@@ -63,97 +63,99 @@ function App() {
   }
 
   return (
-    <div className="app">
-      <main>
-        <div className="searchBox">
-          <input
-            type="text"
-            className="searchBoxInput"
-            placeholder="Enter destination..."
-            onChange={inputHandler}
-            value={query}
-            onKeyPress={FetchLonLat}
-          />
-        </div>
-        {(typeof coord.main != "undefined") ? (
-          <div>
-            <div className="weather">
-              <div className="temperature">
-                <p>{coord.name}, {coord.sys.country}</p>
-                {Math.round(weatherInfo.current.temp)}°c
-                <p>{weatherInfo.current.weather[0].description}</p>
-                <p>hi: {Math.round(coord.main.temp_max)}° lo: {Math.round(coord.main.temp_min)}°</p>
+    <body>
+      <div className="app">
+        <main>
+          <div className="searchBox">
+            <input
+              type="text"
+              className="searchBoxInput"
+              placeholder="Enter destination..."
+              onChange={inputHandler}
+              value={query}
+              onKeyPress={FetchLonLat}
+            />
+          </div>
+          {(typeof coord.main != "undefined") ? (
+            <div>
+              <div className="weather">
+                <div className="temperature">
+                  <p>{coord.name}, {coord.sys.country}</p>
+                  {Math.round(weatherInfo.current.temp)}°c
+                  <p>{weatherInfo.current.weather[0].description}</p>
+                  <p>hi: {Math.round(coord.main.temp_max)}° lo: {Math.round(coord.main.temp_min)}°</p>
+                </div>
               </div>
-            </div>
-            <div className="hourlyForecast">
-              <table>
-                  <tr className="center">
-                    <td>{getTime(1)}</td>
-                    <td className="leftBorder">{getTime(2)}</td>
-                    <td className="leftBorder">{getTime(3)}</td>
-                    <td className="leftBorder">{getTime(4)}</td>
+              <div className="hourlyForecast">
+                <table>
+                    <tr className="center">
+                      <td>{getTime(1)}</td>
+                      <td className="leftBorder">{getTime(2)}</td>
+                      <td className="leftBorder">{getTime(3)}</td>
+                      <td className="leftBorder">{getTime(4)}</td>
+                    </tr>
+                    <tr className="center">
+                      <td>{Math.round(weatherInfo.hourly[1].temp)}°</td>
+                      <td className="leftBorder">{Math.round(weatherInfo.hourly[2].temp)}°</td>
+                      <td className="leftBorder">{Math.round(weatherInfo.hourly[3].temp)}°</td>
+                      <td className="leftBorder">{Math.round(weatherInfo.hourly[4].temp)}°</td>
+                    </tr>
+                    <tr className="center">
+                      <td>{Math.round(weatherInfo.hourly[1].pop)}%</td>
+                      <td className="leftBorder">{Math.round(weatherInfo.hourly[1].pop)}%</td>
+                      <td className="leftBorder">{Math.round(weatherInfo.hourly[2].pop)}%</td>
+                      <td className="leftBorder">{Math.round(weatherInfo.hourly[3].pop)}%</td>
+                    </tr>
+                  </table>
+              </div>
+              <div className="rainhumidity">
+                <table>
+                  <tr>
+                    <td>Chance of rain</td>
+                    <td>Humidity</td>
                   </tr>
-                  <tr className="center">
-                    <td>{Math.round(weatherInfo.hourly[1].temp)}°</td>
-                    <td className="leftBorder">{Math.round(weatherInfo.hourly[2].temp)}°</td>
-                    <td className="leftBorder">{Math.round(weatherInfo.hourly[3].temp)}°</td>
-                    <td className="leftBorder">{Math.round(weatherInfo.hourly[4].temp)}°</td>
-                  </tr>
-                  <tr className="center">
-                    <td>{Math.round(weatherInfo.hourly[1].pop)}%</td>
-                    <td className="leftBorder">{Math.round(weatherInfo.hourly[1].pop)}%</td>
-                    <td className="leftBorder">{Math.round(weatherInfo.hourly[2].pop)}%</td>
-                    <td className="leftBorder">{Math.round(weatherInfo.hourly[3].pop)}%</td>
+                  <tr>
+                    <td className="bigFont">{Math.round(weatherInfo.hourly[1].pop)}%</td>
+                    <td className="bigFont">{Math.round(weatherInfo.hourly[1].humidity)}%</td>
                   </tr>
                 </table>
+              </div>
+              <div className="sunsetrise">
+                <table>
+                  <tr>
+                    <td>Sunrise</td>
+                    <td>Sunset</td>
+                  </tr>
+                  <tr>
+                    <td className="bigFont">{UNIXConverter(1616306442)}</td>
+                    <td className="bigFont">{UNIXConverter(1616350473)}</td>
+                  </tr>
+                </table>
+              </div>
+              <div className="windpressure">
+                <table>
+                  <tr>
+                    <td>Wind</td>
+                    <td>Pressure</td>
+                  </tr>
+                  <tr>
+                    <td className="bigFont">2 mph</td>
+                    <td className="bigFont">1041</td>
+                  </tr>
+                </table>
+              </div>
             </div>
-            <div className="rainhumidity">
-              <table>
-                <tr>
-                  <td>Chance of rain</td>
-                  <td>Humidity</td>
-                </tr>
-                <tr>
-                  <td className="bigFont">{Math.round(weatherInfo.hourly[1].pop)}%</td>
-                  <td className="bigFont">{Math.round(weatherInfo.hourly[1].humidity)}%</td>
-                </tr>
-              </table>
+          ) : 
+          (
+            <div>
+              <div className="enterDestination">
+                Enter destination to get weather information
+              </div>
             </div>
-            <div className="sunsetrise">
-              <table>
-                <tr>
-                  <td>Sunrise</td>
-                  <td>Sunset</td>
-                </tr>
-                <tr>
-                  <td className="bigFont">{UNIXConverter(1616306442)}</td>
-                  <td className="bigFont">{UNIXConverter(1616350473)}</td>
-                </tr>
-              </table>
-            </div>
-            <div className="windpressure">
-              <table>
-                <tr>
-                  <td>Wind</td>
-                  <td>Pressure</td>
-                </tr>
-                <tr>
-                  <td className="bigFont">2 mph</td>
-                  <td className="bigFont">1041</td>
-                </tr>
-              </table>
-            </div>
-          </div>
-        ) : 
-        (
-          <div>
-            <div className="enterDestination">
-              Enter destination to get weather information
-            </div>
-          </div>
-        )}
-      </main>
-    </div>
+          )}
+        </main>
+      </div>
+    </body>
   );
 }
 
